330244accfc5cf579c5a8499d58bfb4324cedf83
admin <dinhvantiendev@gmail.com>
2023-06-25 23:45:35 +0700

    Add front-end (home, search, manga details, read manga, login, signup)

---
+++ StoryFront/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoryFront.Controllers
+{
+	public class AuthController : Controller
+	{
+		public AuthController()
+		{
+		}
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
+
